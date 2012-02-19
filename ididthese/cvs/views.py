4f99f99015c645c26e6303b9be151a3f4d3707a1
Conan Chou <conanchou1412@gmail.com>
2012-02-19 01:31:59 -0500

    get theme setting from db

--- ididthese/cvs/views.py
+++ ididthese/cvs/views.py
@@ -1,12 +1,16 @@
 from django.http import HttpResponse
 from django.template import Template, Context
 from django.template.loader import get_template
+from cvs.models import *
 
 import datetime
+
+user_obj = User.objects.get(user_email="conanchou1412@gmail.com")
+theme = Settings.objects.get(user=user_obj).theme + "/"
 
 def index(request):
     """docstring for index"""
     now = datetime.datetime.now()
-    t = get_template('index.html')
+    t = get_template(theme + 'index.html')
     html = t.render(Context({'current_date': now}))
     return HttpResponse(html)

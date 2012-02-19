0d6755ee4fb37ed7cd32ea08b0fa35a01266ad56
Conan Chou <conanchou1412@gmail.com>
2012-02-18 22:36:22 -0500

    add adming.py

---
+++ ididthese/cvs/admin.py
@@ -0,0 +1,12 @@
+from cvs.models import *
+from django.contrib import admin
+
+admin.site.register(User)
+admin.site.register(Course)
+admin.site.register(Award)
+admin.site.register(Tag)
+admin.site.register(Project)
+admin.site.register(School)
+admin.site.register(Settings)
+admin.site.register(Skill)
+admin.site.register(Work)
